da89f58af206a7191688e02352c193abf38e8581
shahronak1996@gmail.com <Ronak@MacBook-Air-2.local>
2017-10-05 18:41:53 -0500

    Initial commit

---
+++ Capp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Capp
+//
+//  Created by Ronak Shah on 10/5/17.
+//  Copyright © 2017 Team6. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
