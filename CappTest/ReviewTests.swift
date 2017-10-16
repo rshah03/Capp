8ad3568c56e7039a385ebec7cceed0b6a1431fee
shahronak1996@gmail.com <Ronak@MacBook-Air-2.local>
2017-10-15 23:22:35 -0500

    Reviews - Tests for rating & summary

---
+++ CappTest/ReviewTests.swift
@@ -0,0 +1,49 @@
+//
+//  ReviewTests.swift
+//  CappTest
+//
+//  Created by Ronak Shah on 10/15/17.
+//  Copyright © 2017 Team6. All rights reserved.
+//
+
+import XCTest
+
+class ReviewTests: XCTestCase {
+    
+    var review = Review()
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testSetRatingWhenRatingIsZero() {
+        review.setRating(rating: 0)
+        XCTAssertEqual(review.getRating(), 1)
+    }
+    
+    func testSetRating() {
+        review.setRating(rating: 5)
+        XCTAssertEqual(review.getRating(), 5)
+    }
+    
+    func testSetReviewSummary() {
+        let text = "Lorem Ipsum"
+        review.setReviewSummary(summary: "Lorem Ipsum")
+        XCTAssertEqual(review.getReviewSummary(), text)
+    }
+    
+    
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
