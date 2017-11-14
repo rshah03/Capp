306d50c47d558a21bb83b132a3e689239c0482e6
Ronak Shah <shahronak1996@gmail.com>
2017-11-13 21:43:53 -0600

    Recovery update

---
+++ Capp/Settings/Settings.swift
@@ -0,0 +1,13 @@
+//
+//  Settings.swift
+//  Capp
+//
+//  Created by Chuck Argyle on 10/31/17.
+//  Copyright © 2017 Team6. All rights reserved.
+//
+
+import Foundation
+
+class Settings : UserDefaults {
+    var rememberSearchTags : Bool = false
+}
